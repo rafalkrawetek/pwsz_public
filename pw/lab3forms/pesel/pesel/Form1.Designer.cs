aa85ceb1bf1a87ad53d10f8d0975a8c6148f8f54
rafalkrawetek <rafal.krawetek@gmail.com>
2020-03-08 21:41:30 +0100

    pw lab3 pesel add

---
+++ pw/lab3forms/pesel/pesel/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace pesel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pesel = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.wynik = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(216, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(363, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sprawdzanie numeru PESEL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(110, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PESEL";
+            // 
+            // pesel
+            // 
+            this.pesel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.pesel.Location = new System.Drawing.Point(182, 182);
+            this.pesel.Name = "pesel";
+            this.pesel.Size = new System.Drawing.Size(151, 23);
+            this.pesel.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(498, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 43);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Sprawdź";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.Location = new System.Drawing.Point(514, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 34);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Wyczyść";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // wynik
+            // 
+            this.wynik.AutoSize = true;
+            this.wynik.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.wynik.Location = new System.Drawing.Point(66, 349);
+            this.wynik.Name = "wynik";
+            this.wynik.Size = new System.Drawing.Size(0, 31);
+            this.wynik.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.wynik);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pesel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pesel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label wynik;
+    }
+}
+
---
+++ pw/lab3forms/pesel/pesel/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace pesel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] mnoznik = new int[] { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 0 };
+            try
+            {
+                if (pesel.Text.Length == 11)
+                {
+                    int suma = 0;
+                    for(int i = 0; i < 10; i++)
+                    {
+                        if (pesel.Text[i] < 48 || pesel.Text[i] > 57)
+                        {
+                            Debug.WriteLine(pesel.Text[i]);
+                            throw new Exception();
+                        }
+                        suma += (pesel.Text[i]-48)*mnoznik[i];
+                    }
+                    if ((10 - suma % 10) == (pesel.Text[10] - 48))
+                    {
+                        wynik.ForeColor = Color.Green;
+                        wynik.Text = "Wprowadzony pesel jest prawidłowy!";
+                    }
+                    else
+                    {
+                        wynik.ForeColor = Color.Red;
+                        wynik.Text = "Wprowadzony pesel jest nieprawidłowy!";
+                    }
+                }
+                else
+                {
+                    wynik.ForeColor = Color.Red;
+                    wynik.Text = "Wprowadź pesel o prawidłowej długości (11 cyfr)!";
+                }
+
+            }
+            catch
+            {
+                wynik.ForeColor = Color.Red;
+                wynik.Text = "Wprowadź poprawne dane (11 cyfr)!";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pesel.Text = "";
+            wynik.Text = "";
+        }
+    }
+}
