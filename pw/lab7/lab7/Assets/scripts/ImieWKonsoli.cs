58c235929d35d9eca7d1820ef92f95f7495570e2
rafalkrawetek <rafal.krawetek@gmail.com>
2020-04-27 16:37:26 +0200

    pw lab 7 add

---
+++ pw/lab7/lab7/Assets/scripts/ImieWKonsoli.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImieWKonsoli : MonoBehaviour
+{
+    public int ilosc = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < ilosc; i++)
+            print("Rafał");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
