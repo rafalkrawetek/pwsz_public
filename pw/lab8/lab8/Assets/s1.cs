1c4bfb1bfae855c6899dc29933262dcf8025e917
rafalkrawetek <rafal.krawetek@gmail.com>
2020-05-14 15:55:31 +0200

    pw lab8 add

---
+++ pw/lab8/lab8/Assets/s1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] kule = GameObject.FindGameObjectsWithTag("kula");
+        foreach (GameObject kula in kule)
+            Destroy(kula);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
